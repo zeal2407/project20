922c55d048961edadf48f9b994b00fb00471318c
zeal2407 <77246154+zeal2407@users.noreply.github.com>
2021-02-07 15:20:28 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -5,7 +5,7 @@ function setup() {
   speed=random(55,90);
   weight=random(400,1500);
   car=createSprite(50,200,50,50);
-  wall=createSprite(1500,200,60,height/2);
+  wall=createSprite(1200,200,60,height/2);
   wall.shapeColor=(0,255,0);
   
 }
